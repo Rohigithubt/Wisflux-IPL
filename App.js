296121c6bc40e73b3c1d262bc54934352dad18a2
Rohit jain <115576916+Rohigithubt@users.noreply.github.com>
2025-01-01 20:53:41 +0530

    .

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Visitors from './components/Visitors';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Visitors />
+    </div>
+  );
+};
+
+export default App;
